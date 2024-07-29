00894aa7bf0114ec3ef19228770d115aa43d4056
Ansth <anujsinghth@gmail.com>
2024-07-29 14:22:58 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, make_response, jsonify
+from model import db
+app = Flask(__name__)
+
+
+@app.route('/hello')
+def abcd():  # put application's code here
+    return 'Hello dlkdlkdld!'
+@app.route('/api/products',methods = ['GET'])
+def getAllProducts():  # put application's code here
+    return make_response(jsonify({"products": db}), 200)
+
+
+if __name__ == '_main_':
+    app.run()
